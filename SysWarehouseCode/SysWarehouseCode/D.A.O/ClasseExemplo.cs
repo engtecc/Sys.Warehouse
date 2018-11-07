cd529e6f68e5fd3b315448617f47fbba342bfe8c
jean <jeangustavorezende@gmail.com>
2018-11-07 16:24:16 -0200

    Pasta para o codigo fonte

---
+++ SysWarehouseCode/SysWarehouseCode/D.A.O/ClasseExemplo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SysWarehouseCode.D.A.O
+{
+    class ClasseExemplo
+    {
+    }
+}
