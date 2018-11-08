6b067a1106af735853b020cfea949d4d362432f4
jean <jeangustavorezende@gmail.com>
2018-11-08 20:01:51 -0200

    telas

---
+++ SysWarehouseCode/SysWareHouseCode/View/Venda.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SysWareHouseCode.View
+{
+    public partial class VendaRapida : Form
+    {
+        public VendaRapida()
+        {
+            InitializeComponent();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Venda janela = new Venda();
+            janela.ShowDialog();
+        }
+    }
+}
