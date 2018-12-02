076a01936d3b47acb9179e42f221798373e39553
Filipe Oliveira <40152131+filipeoliveiragon@users.noreply.github.com>
2018-12-02 16:35:00 -0200

    Update Máscaras

---
+++ warehouse/js/maskscript.js
@@ -0,0 +1,25 @@
+﻿$(document).ready(function() {
+    $("#txtCpf").mask("000.000.000-00")
+    $("#txtTelefone").mask("(00) 0000-00009")
+    $("#txtTelefone").blur(function(event){
+        if($(this).val().length == 15){
+            $("#txtTelefone").mask("(00) 00000-0009")
+        } else{
+            $("#txtTelefone").mask("(00) 0000-00009")
+        }
+    })
+    $("#txtNumero").mask("999990A", {
+        translation: {
+            A: {
+                pattern: /[0-9A-Za-z]/
+            }
+        }, reverse: true
+    })
+    $('#txtRG').mask("999.999.999-X",{
+        translation: {
+            X: {
+                pattern: /[X0-9]/
+            }
+        }, reverse: true
+    })
+});
